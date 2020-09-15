042945d8333a1f1d82daec52dd9992f51d9f5ac2
Maitreyee <maitreyee.rao@gmail.com>
2020-09-15 03:11:00 -0600

    Add files via upload

---
+++ test_cbfifo.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include"cbfifo.h"
+
+int test_enqueue_dequeue();
+int test_dequeue();
+
+int test_enqueue_dequeue()
+{
+  size_t test[20];
+  char str[] = "universityofcoloradoboulder";
+  char buff[126] = { 1, 2, 3, 4, 5, 6, 7 };
+  int buff1[500] = { 2, -2, 30, 40, 60, 700 };
+  int buff2[20] = { 0x30, 0x40, 0xffff, 0x0000
+  };
+  int test_enqueue_success = 0;
+  size_t len;
+ 	//enqueue 10 bytes data
+  test[0] = cbfifo_enqueue(str, 10);
+  test[1] = cbfifo_enqueue(buff, 0);
+        //check enqued bytes
+  printf("\n enqued byte %lu",test[1]);
+ 	//dequeue 10 bytes data
+  test[2] = cbfifo_dequeue(buff, 10);
+
+ 	//check if current length has become 0;
+  len = cbfifo_length();
+  printf("\n length is %lu", len);
+  if (len == 0)
+    test_enqueue_success++;
+
+  test[3] = cbfifo_enqueue(buff, 30);
+  test[4] = cbfifo_enqueue(buff1, 15);
+  test[5] = cbfifo_dequeue(buff, 10);
+ 	//length check
+  len = cbfifo_length();
+  printf("\n length is %lu", len);
+
+  test[6] = cbfifo_enqueue(buff1, -10);
+  test[7] = cbfifo_enqueue(buff2, 20);
+  test[8] = cbfifo_enqueue(str, 0);
+  test[9] = cbfifo_enqueue(str, 40);
+  test[10] = cbfifo_enqueue(buff, 5);
+
+  test[11] = cbfifo_enqueue(buff2, 100);
+  test[12] = cbfifo_dequeue(buff, 156);
+  len = cbfifo_length();
+  if (len == 0)
+    test_enqueue_success++;
+  printf("\n length is %lu", len);
+
+  test[13] = cbfifo_enqueue(buff, 5);
+  test[14] = cbfifo_dequeue(buff, 10);
+
+  test[15] = cbfifo_enqueue(buff, 100);
+  for (int i = 0; i < 9; i++)
+  {
+    if (test[i] > 0)
+    {
+      test_enqueue_success++;
+    }
+  }
+
+  return (test_enqueue_success);
+
+}
+
+int test_cbfifo()
+{
+  int current_length, total_capacity;
+  int test_passed;
+  test_passed = test_enqueue_dequeue();
+
+  printf("\n No of tests passed  %d", test_passed);
+  current_length = cbfifo_length();
+  printf("\n %d", current_length);
+  total_capacity = cbfifo_capacity();
+  printf("\n %d", total_capacity);
+
+  return (0);
+
+}
---
+++ test_llfifo.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "llfifo.h"
+
+int test_llfifo_enqueue(llfifo_t *fifo)
+{
+  int totallength, currentcapacity;
+  int currentlength;
+  char str[] = "sleepy";
+  char str1[] = "sad";
+  char str2[] = "maitreyee";
+  char str3[] = "cold";
+  char str4[] = "boulder";
+  currentcapacity = llfifo_capacity(fifo);
+ // printf("\n Current Capacity %d", currentcapacity);
+  currentlength = llfifo_enqueue(fifo, str);
+  currentlength = llfifo_enqueue(fifo, str1);
+  currentlength = llfifo_enqueue(fifo, str2);
+  currentlength = llfifo_enqueue(fifo, str3);
+  currentlength = llfifo_enqueue(fifo, str4);
+
+ 	//check if the capacity initalised is less than elements enqued and test if the enque lets add more nodes
+  if (currentcapacity < currentlength)
+  {
+
+    currentcapacity = llfifo_capacity(fifo);
+    totallength = llfifo_length(fifo);
+    //printf("\n total length is %d", totallength);
+    //printf("\n Capacity after enqueue %d", currentcapacity);
+    if (currentcapacity == totallength)
+      return 0;
+  }
+
+  if ((currentlength != -1))	// check if list is not empty &first element entered is same as first enqueued
+    return 0;
+
+  else
+    return -1;
+
+}
+
+int test_fifo()
+{
+
+  int capacity[] = { -1, 2, 4, 20, 5, 3, 1, 0 };
+  int  currentcapacity;
+  int test_success = 0;
+  int test1;
+  llfifo_t * list1;
+
+  for (int i = 0; i < 7; i++)
+  {
+
+    list1 = llfifo_create(capacity[i]);
+    currentcapacity = llfifo_capacity(list1);
+    if (currentcapacity == capacity[i])
+    {
+      printf("\nPassed for capacity %d", capacity[i]);
+    }
+
+    test1 = test_llfifo_enqueue(list1);
+
+    if ((test1 == 0))
+      test_success++;
+  }
+
+  printf("test success %d", test_success);
+
+  return (0);
+}
+
+int test_dequeue()
+{
+  int   currentcapacity, length;
+  int test_success = 0;
+  llfifo_t * list1;
+  void *el, *t, *p;
+  char str[] = "sleepy";
+  char str1[] = "run";
+  char str2[] = "bat";
+  length = 10;
+  list1 = llfifo_create(length);
+  llfifo_enqueue(list1, str);
+  llfifo_enqueue(list1, str1);
+  llfifo_enqueue(list1, str2);
+  //totallength = llfifo_length(list1);
+   
+  currentcapacity = llfifo_capacity(list1);
+
+  el = llfifo_dequeue(list1);
+  t = llfifo_dequeue(list1);
+  p = llfifo_dequeue(list1);
+ 	//check order of dequeue
+  if ((el == str) && (t == str1) && (p == str2))
+  {
+    currentcapacity = llfifo_capacity(list1);
+    if (currentcapacity == length)	//check if capcity hasnt shrunk
+     { 
+        test_success++;
+      }   
+
+   }
+   printf("test success %d", test_success);
+
+llfifo_destroy(list1);
+return (test_success);
+}
